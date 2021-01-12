bbc27e680c585f0bc6fa08c90b8e343ebcba25e6
mmirajaahmed <mmiraj.aahmed@gmail.com>
2021-01-12 15:28:12 +0530

    print change

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
-public class hello{
+public class Hello{
 
 public static void main(String args[])
 {
 
-System.out.println("HI this is Jenkin Git Demo");
+System.out.println("HI this is Jenkin Git Demo1");
 
 }
 }
